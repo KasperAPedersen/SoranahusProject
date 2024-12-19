a574d7e3c7202d00d8f112b7c7abca8b008f1833
Kasper Doe <kasper@doe.dk>
2024-12-19 09:41:50 +0100

    Added staff page Fixed links in header

--- routes/common.js
+++ routes/common.js
@@ -2,12 +2,16 @@ import Router from 'express';
 
 const route = new Router();
 
-route.get('/', (req, res) => {
-    res.render('index.ejs');
-});
-
 route.get('/gallery', (req, res) => {
     res.render('gallery.ejs');
+});
+
+route.get('/staff', (req, res) => {
+    res.render('staff.ejs');
+});
+
+route.get('/', (req, res) => {
+    res.render('index.ejs');
 });
 
 export default route;
