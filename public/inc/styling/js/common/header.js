41c2fe407fa47e74713179a487f26461b9abeac6
Kasper Doe <kasper@doe.dk>
2024-12-19 11:41:50 +0100

    Added mobile navigation dropdown

--- public/inc/styling/js/common/header.js
+++ public/inc/styling/js/common/header.js
@@ -12,3 +12,21 @@ let toggleMobileNavigation = () => {
     let modal = document.getElementById('mobileModal');
     modal.open ? modal.close() : modal.showModal();
 }
+
+let changeMobileModal = (menu) => {
+    let modals = document.querySelectorAll('.mobileNavigationModal');
+    for(let i = 0; i < modals.length; i++) {
+        modals[i].style.display = "none";
+    }
+
+    switch(menu){
+        case "staff":
+            document.querySelector('.staffModal').style.display = "grid";
+            document.querySelector('.mobileModalBackArrow').style.visibility = "visible";
+            break;
+        default:
+            document.querySelector('.mobileModalBackArrow').style.visibility = "hidden";
+            document.querySelector('.defaultModal').style.display = "grid";
+            break;
+    }
+}
